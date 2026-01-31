c304ad3102c942dcd23d876d50e85710808ec37d
Issleo <lffloresp@hotmail.com>
2026-01-30 22:21:36 -0500

    Segundo commit

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -81,7 +81,17 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+#Conexion a possgress
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': '',
+#        'USER': '',
+#        'PASSWORD': '',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
